efebb56a4f701113411d0590031b82c9f9a7630a
Shane <fullstack.fever@gmail.com>
2020-03-17 08:04:15 -0500

    airbnb clone app with react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Flat from "./flat";
+// import logo from './logo.svg';
+import "./App.css";
+class App extends React.Component {
+  render() {
+    const flat = {};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div>
+        <Flat flat={flat} />
+      </div>
+    );
+  }
 }
 
 export default App;
